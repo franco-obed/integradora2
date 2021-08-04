e3bf3c687c8d7fc3645e9ab82d2315dfd503e992
Obed Franco <obed.franco@autozone.com>
2021-08-04 13:51:55 -0600

    Se enlistan los componentes que van a usarse en la aplicacion. > Se agregaron los nuevos componentes: BienvenidoComponent y NuevaSesionComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { NuevaSesionComponent } from './nueva-sesion/nueva-sesion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    BienvenidoComponent,
+    NuevaSesionComponent
   ],
   imports: [
     BrowserModule,
