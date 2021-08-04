282c5343d57a1ff85431d0d9017432d0f2f3890e
Obed Franco <obed.franco@autozone.com>
2021-08-04 13:54:25 -0600

    El modulo ruteador permite la navegacion de vista a vista de manera asincrona dentro de la aplicacion. El arreglo routes recibe todas las rutas a los componentes
    > Se agregan rutas para la vista de bienvenida y login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NuevaSesionComponent } from './nueva-sesion/nueva-sesion.component';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: AppComponent},
+  {
+    path: '', component: BienvenidoComponent,
+  },
+  {
+    path: 'welcome', component: NuevaSesionComponent,
+  },
+  {
+    path: 'login', component: LoginComponent
+  }
+
+];
 
 @NgModule({
+
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
