dc581f37a1088c2a97827e99db02f09bf9456f74
Obed Franco <obed.franco@autozone.com>
2021-08-15 21:25:36 -0600

    Componente que muestra la leyenda "Bajo"

---
+++ src/app/lectura/nivel-humedad-bajo/nivel-humedad-bajo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nivel-humedad-bajo',
+  templateUrl: './nivel-humedad-bajo.component.html',
+  styleUrls: ['./nivel-humedad-bajo.component.css']
+})
+export class NivelHumedadBajoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
