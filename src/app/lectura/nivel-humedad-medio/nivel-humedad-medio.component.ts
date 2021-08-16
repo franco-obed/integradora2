047e288c8117a5c245af0dbc9616080623532097
Obed Franco <obed.franco@autozone.com>
2021-08-15 21:27:01 -0600

    Componente que muestra la leyenda "Medio"

---
+++ src/app/lectura/nivel-humedad-medio/nivel-humedad-medio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nivel-humedad-medio',
+  templateUrl: './nivel-humedad-medio.component.html',
+  styleUrls: ['./nivel-humedad-medio.component.css']
+})
+export class NivelHumedadMedioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
