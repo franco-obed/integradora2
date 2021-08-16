3a5e1c40a81c0185c395a18edd2bba35a1366a32
Obed Franco <obed.franco@autozone.com>
2021-08-15 21:13:29 -0600

    Componente html y css que muestra el porcentaje de humedad bajo en color rojo

---
+++ src/app/lectura/porcentaje-humedad-bajo/porcentaje-humedad-bajo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-porcentaje-humedad-bajo',
+  templateUrl: './porcentaje-humedad-bajo.component.html',
+  styleUrls: ['./porcentaje-humedad-bajo.component.css']
+})
+export class PorcentajeHumedadBajoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
